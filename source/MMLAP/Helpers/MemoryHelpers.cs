870b3d1e88da53040516bec6cfaf27396d11c06f
functorial <alexpokorny92@gmail.com>
2026-03-19 00:04:12 -0700

    Add memory helpers for readability

---
+++ source/MMLAP/Helpers/MemoryHelpers.cs
@@ -0,0 +1,22 @@
+﻿using Archipelago.Core.Util;
+using MMLAP.Models;
+using static MMLAP.Models.MMLEnums;
+using System;
+using System.Collections.Generic;
+using ImGuiNET;
+
+namespace MMLAP.Helpers
+{
+    public class MemoryHelpers
+    {
+        public static bool IsInTitleScreen()
+        {
+            return Memory.ReadByte(Addresses.TitleScreen.Address) == 0x20;
+        }
+
+        public static bool IsInGameOrCutscene()
+        {
+            return Memory.ReadByte(Addresses.TitleScreen.Address) == 0xA4;
+        }
+    }
+}
