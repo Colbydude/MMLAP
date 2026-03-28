ea8e70ef5fe5fdb470d7e9bad3ee1bb2a4f33330
functorial <alexpokorny92@gmail.com>
2026-03-27 18:03:34 -0700

    Bump version to 0.2.5

--- source/MMLAP/App.axaml.cs
+++ source/MMLAP/App.axaml.cs
@@ -36,8 +36,9 @@ namespace MMLAP;
 public partial class App : Application
 {
     // TODO: Remember to set this in MMLAP.Desktop as well.
-    public static readonly string Version = "0.2.4";
-    public static readonly List<string> SupportedVersions = ["0.2.0", "0.2.1", "0.2.2", "0.2.3", "0.2.4"];
+    public static readonly string Version = "0.2.5";
+    public static readonly List<string> SupportedVersions = ["0.2.0", "0.2.1", "0.2.2", "0.2.3", "0.2.4", "0.2.5"];
+
 
     public static MainWindowViewModel? Context;
     public static ArchipelagoClient? APClient { get; set; }
